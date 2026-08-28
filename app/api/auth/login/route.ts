229538a396977d3d25bd3bc34f217d9ffb5a3834
movielegend <137597832+movielegend@users.noreply.github.com>
2026-08-28 17:36:21 +0700

    fix(vercel): add prisma generate script to package.json and force-dynamic to login route

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import { comparePassword, hashPassword, signJWT } from '@/lib/auth';
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(request: Request) {
   try {
     const { email, password } = await request.json();
